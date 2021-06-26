d400f83d99ee215d6c2e24bbb234aa9be57ce506
TheOfficer2303 <76702547+TheOfficer2303@users.noreply.github.com>
2021-06-26 20:44:20 +0200

    Add files via upload

--- KUD Tamburica/server.js
+++ KUD Tamburica/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 var path = require('path');
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session)
+const db = require('./db')
+
 
 const homeRouter = require('./routes/home.routes');
 const newsRouter = require('./routes/news.routes');
+const loginRouter = require('./routes/login.routes');
+const logoutRouter = require('./routes/logout.routes');
+const signupRouter = require('./routes/signup.routes');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,7 +19,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true}));
 
+app.use(session({
+    store: new pgSession({
+        pool: db.pool
+    }),
+    secret: "something",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    }
+})
+)
+
+
 app.use('/', homeRouter)
 app.use('/addnews', newsRouter);
-//Aaaaaaaaaaaa
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter)
+app.use('/signup', signupRouter);
+
 app.listen(80);
